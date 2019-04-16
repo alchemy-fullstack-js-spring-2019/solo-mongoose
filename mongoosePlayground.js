a60c3f3df3c03d621e5ebf7f796ef06ea23fb8f6
Dave Cornelius <d_cornelius94@yahoo.com>
2019-04-16 11:58:50 -0700

    was able to create a tweet

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -1,3 +1,24 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('')
+mongoose.connect('mongodb://localhost:27017/tweets', {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const tweetSchema = new mongoose.Schema
+({
+  handle: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+Tweet
+  .create({ handle: 'dave', text: 'hopefully this works' })
+  .then(createdTweet => console.log(createdTweet));
