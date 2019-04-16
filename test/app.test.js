7e94f76d9bf4ea31fa2f82b69286c6c8c1cc1bce
Cara Ponzini <ponzini@gmail.com>
2019-04-16 14:55:22 -0700

    passed all tests for tweets rout

--- test/app.test.js
+++ test/app.test.js
@@ -63,4 +63,43 @@ describe('tweet routes', () => {
         });
       });
   });
+
+  it('can find a tweet by id and update', () => {
+    return Tweet
+      .create({ handle: 'cara', text: 'here is some text' })
+      .then(newTweet => {
+        return request(app)
+          .patch(`/tweets/${newTweet._id}`)
+          .send({
+            handle: 'cara',
+            text: 'new text new text'
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          handle: 'cara', 
+          text: 'new text new text', 
+          _id: expect.any(String),
+          __v: 0 
+        });
+      });
+  });
+
+  it('can delete a tweet by id', () => {
+    const aTweet = { handle: 'cara', text: 'here is some text' };
+    return Tweet
+      .create(aTweet)
+      .then(newTweet => {
+        return request(app)
+          .delete(`/tweets/${newTweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          handle: 'cara', 
+          text: 'here is some text', 
+          _id: expect.any(String),
+          __v: 0 
+        });
+      });
+  });
 });
