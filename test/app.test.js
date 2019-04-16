f5d3fc56ca0d171ee596703a3ba2cd71e9575c1f
Cara Ponzini <ponzini@gmail.com>
2019-04-16 14:31:42 -0700

    test passd for find and find by id

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../lib/app');
-// const Tweet = require('../lib/models/Tweet');
+const Tweet = require('../lib/models/Tweet');
 
 describe('tweet routes', () => {
   beforeAll(() => {
@@ -31,6 +31,35 @@ describe('tweet routes', () => {
           text: 'tweets have text',
           _id: expect.any(String),
           __v: 0
+        });
+      });
+  });
+
+  it('can get all the tweets', () => {
+    return Tweet
+      .create({ handle: 'cara', text: 'I am a tweet' })
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
+
+  it('can get a tweet by id', () => {
+    return Tweet
+      .create({ handle: 'cara', text: 'here is some text' })
+      .then(newTweet => {
+        return request(app)
+          .get(`/tweets/${newTweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          handle: 'cara', 
+          text: 'here is some text', 
+          _id: expect.any(String),
+          __v: 0 
         });
       });
   });
