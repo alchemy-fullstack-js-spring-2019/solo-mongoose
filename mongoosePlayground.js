09881b918e53d4ef204bda83d1b0eb41d906313a
emily baier <emilyrbaier@gmail.com>
2019-04-16 11:55:34 -0700

    created mongoose model and schema, created tweet

---
+++ mongoosePlayground.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/tweets', {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const tweetSchema = new mongoose.Schema({
+  handle: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+Tweet
+  .create({ handle: 'emily', text: 'my cool tweet' })
+  .then(createdTweet => console.log(createdTweet));
