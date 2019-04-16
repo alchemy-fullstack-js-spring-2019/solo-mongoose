1df71a7c71bb0788a06095c75d5182b1b2d59eed
Cheri Portman <cheriportman@hotmail.com>
2019-04-16 16:56:56 -0700

    adding heroku

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-require('dotenv').config;
+require('dotenv').config();
 
 const mongoose = require('mongoose');
 const app = require('./lib/app');
 
-mongoose.connect('mongodb://localhost:27017/tweets', {  //(PROCESS.ENV.mongodb_uri, {......
+mongoose.connect(process.env.MONGODB_URI, {
   useFindAndModify: false,
   useNewUrlParser: true,
   useCreateIndex: true
 });
 
-app.listen(7890, () => {
-  console.log('Started on port 7890');
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
