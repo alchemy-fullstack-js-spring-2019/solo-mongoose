5e10cd26b6083ed7b93871cbb13ae47a897a4f8e
Cara Ponzini <ponzini@gmail.com>
2019-04-16 13:47:53 -0700

    created test in app

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../lib/app');
+const Tweet = require('../lib/models/Tweet');
 
 describe('tweet routes', () => {
   beforeAll(() => {
@@ -13,5 +14,21 @@ describe('tweet routes', () => {
   });
   afterEach(() => {
     return mongoose.connection.close();
+  });
+
+  it('can create a new tweet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        handle: 'cara',
+        text: 'tweets have text'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'cara',
+          text: 'tweets have text',
+          _id: expect.any(String)
+        });
+      });
   });
 });
