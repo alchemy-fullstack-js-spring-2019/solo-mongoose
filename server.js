0f35c7c46362354c7030c34faf895d341890e237
Parker Helland <hellandp@gmail.com>
2019-04-16 15:49:16 -0700

    added env stuff

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+const app = require('./lib/app');
 
-mongoose.connect('mongodb://localhost:27017/ninjas', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true
-}, console.log('√ Server Started √'));
+});
+
+const PORT = process.env.PORT || 5678;
+
+app.listen(PORT, () => {
+  console.log(`√ Server Started on: ${PORT} √`);
+});
