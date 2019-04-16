faa8871a1a5fb2813c2ec8ab95a4e51b6ce1166d
Sean Nelson <smnelsonn@gmail.com>
2019-04-16 14:35:52 -0700

    heroku setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./lib/app.js');
 
+const PORT = process.env.PORT || 8888;
 
 mongoose.connect('mongodb://127.9.9.1:27017/tweets', {
   useNewUrlParser: true,
