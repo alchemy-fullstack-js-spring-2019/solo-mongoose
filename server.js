04ab868a096d6328c3769d09a4c133f0e7e3c44e
emily baier <emilyrbaier@gmail.com>
2019-04-16 13:51:55 -0700

    app and server created

---
+++ server.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { app } = require('./lib/app');
+
+
+mongoose.connect('mongodb://localhost:27017', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+app.listen(7890, ()=> {
+  console.log('server started on port 7890');
+});
