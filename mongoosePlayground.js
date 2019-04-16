a5b4f5c8e15496822a9d3f7692d0c7e4307099a5
Cheri Portman <cheriportman@hotmail.com>
2019-04-16 11:51:13 -0700

    initial build... and playground started, oops....

---
+++ mongoosePlayground.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/tweets', {
+  useNewUrlParser: true,
+  useFindAndModify: false  
+});
+
+const tweetSchema = new mongoose.Schema({
+  handle: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
