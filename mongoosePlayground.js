57a923b1f663647d1835d590ff1da9792ecca015
Anna Ridenour <annamridenour@gmail.com>
2019-04-16 11:57:00 -0700

    completed mongoosePlayground

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -37,24 +37,50 @@ const tweetsSchema = new mongoose.Schema({
 
 const Tweet = mongoose.model('Tweet', tweetsSchema);   //creating a tweet model by saying what it should be called and what schema is should use
 
-Tweet
-    .create({ handle: 'ryan', body: 'my first tweet' })
-    .then(createdTweet => {
-        return Tweet.findById(createdTweet._id);
-    })
-    .then(foundTweet => console.log(foundTweet));
+// Tweet
+//     .create({ handle: 'ryan', body: 'my first tweet' })
+//     .then(createdTweet => {
+//         return Tweet.findById(createdTweet._id);
+//     })
+//     .then(foundTweet => console.log(foundTweet));
+//     .finally(() => {
+//         mongoose.connection.close();     //ends your interaction (before this, you were having to ctrl c in the terminal to be able to enter new stuff)
+//     });
+
+// Tweet
+//     .find()
+//     .then(tweets => console.log(tweets));
+//     .finally(() => {
+//         mongoose.connection.close();     //ends your interaction (before this, you were having to ctrl c in the terminal to be able to enter new stuff)
+//     });
+
+// Tweet
+//     .findById('5cb61a65696e02546e1c587e')
+//     .then(foundTweet => console.log(foundTweet))
+//     .finally(() => {
+//         mongoose.connection.close();     //ends your interaction (before this, you were having to ctrl c in the terminal to be able to enter new stuff)
+//     });
+
+// Tweet
+//     .findByIdAndUpdate('5cb61a65696e02546e1c587e', { handle: 'spot' }, { new: true })
+//     .then(updatedTweet => console.log(updatedTweet))
+//     .finally(() => {
+//         mongoose.connection.close(); 
+//     });
+
+// Tweet
+//     .create({ handle: 'anna', body: 'tweet to update' })
+//     .then(createdTweet => {
+//         return Tweet.findByIdAndUpdate(createdTweet._id, { body: 'updated' }, { new: true });
+//     })
+//     .then(updatedTweet => console.log(updatedTweet))
+//     .finally(() => {
+//         mongoose.connection.close(); 
+//     });
 
 Tweet
-    .findById('5cb61a65696e02546e1c587e')
-    .then(foundTweet => console.log(foundTweet))
+    .findByIdAndDelete('5cb619f3371ca5541a6fc4ef')
+    .then(deleted => console.log(deleted))
     .finally(() => {
-        mongoose.connection.close();     //ends your interaction (before this, you were having to ctrl c in the terminal to be able to enter new stuff)
+        mongoose.connection.close();
     });
-
-Tweet
-    .findByIdAndUpdate('5cb61a65696e02546e1c587e', { handle: 'spot' }, { new: true })
-    .then(updatedTweet => console.log(updatedTweet))
-    .finally(() => {
-        mongoose.connection.close(); 
-    });
-
