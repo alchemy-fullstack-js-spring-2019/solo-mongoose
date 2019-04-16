87f3b50e8728e24f74ee3117b52d481e8eeedc04
mcnadams <mcneilbo@oregonstate.edu>
2019-04-16 11:07:15 -0700

    write a tweet to mongodb

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -1,1 +1,29 @@
 const mongoose = require('mongoose');
+
+// connect to mongodb database
+mongoose.connect('mongobd://localhost:27017/tweets', { useNewUrlParser: true });
+
+// create tweet schema
+const tweetSchema = new mongoose.Schema({
+    handle: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    }
+});
+
+// create tweet model
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+// create a new tweet
+Tweet
+    .create({ 
+        handle: 'bonnie', 
+        text: 'my first tweet'
+    })
+    .then(createdTweet => console.log(createdTweet));
+
+
