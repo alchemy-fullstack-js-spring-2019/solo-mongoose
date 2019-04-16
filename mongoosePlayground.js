686a670a7cc89713de18cddb6a122e94eb87d106
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-16 11:59:17 -0700

    findByIdAndUpdate

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -19,13 +19,27 @@ const tweetsSchema = new mongoose.Schema({
 
 const Tweet = mongoose.model('Tweet', tweetsSchema);
 
+// Tweet
+//   .create({ handle: 'meg', body: 'I love tweeting' })
+//   .then(createdTweet => {
+//     return Tweet.findById(createdTweet._id);
+//   })
+//   .finally(() => {
+//     mongoose.connection.close();
+//   });
+
+// Tweet
+//   .create({ handle: 'bob', body: 'I love tweeting more than Meg' })
+//   .then(createdTweet => {
+//     return Tweet.findById(createdTweet._id);
+//   })
+//   .finally(() => {
+//     mongoose.connection.close();
+//   });
+
 Tweet
-  .create({ handle: 'meg', body: 'I love tweeting' })
-  .then(createdTweet => {
-    return Tweet.findById(createdTweet._id);
-  })
+  .findByIdAndUpdate('5cb62523bb245e20dda9a7d8', { handle: 'Megan' }, { new: true })
+  .then(updatedTweet => console.log(updatedTweet))
   .finally(() => {
-    
-  })
-
-
+    mongoose.connection.close();
+  });
