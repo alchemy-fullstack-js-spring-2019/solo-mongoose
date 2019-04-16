f2d22684d87f732dd2c127a0e627420f568ec426
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-04-16 14:08:16 -0700

    only have handle test passing

--- test/models/tweetSchema.test.js
+++ test/models/tweetSchema.test.js
@@ -14,4 +14,12 @@ describe('tests the "properties", you could say, of the tweet schema/tweet model
       //why not string again?
     });
   });
+  it('only has a handle, thus resulting in path "body" required error', () => {
+    const tweet = new TweetSchema({
+      handle: 'intro_mode'
+    });
+    const errors = tweet.validateSync().errors;
+    //errors.body.message because since we dont have a body, the error message will be in the body object. assuming its an object within an errors object
+    expect(errors.body.message).toEqual('Path `body` is required.');
+  });
 });
