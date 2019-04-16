804731a289ba84030832762dc932bbf2c8125578
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-16 15:33:09 -0700

    finished setting up server?

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+require('dotenv').config();
 const app = require('./lib/app');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true, useFindAndModify: false,
+  useCreateIndex: true
+});
+
+const PORT = process.env.PORT || 7890
+
 
 app.listen(7890, () => {
-  console.log('Server is Listening!');
+  console.log(`Server is Listening on ${PORT}!`);
 });
 
