5ae0fb80cca401b7a85531f8910af25e32c75512
Masmics <cherlocker3113@gmail.com>
2019-04-16 12:45:13 -0700

    tweet added

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -4,3 +4,20 @@ mongoose.connect('mongodb://127.0.0.1:27017', {
   useNewUrlParser: true
 });
 
+const tweetSchema = new mongoose.Schema({
+  handle: {
+    type: String,
+    required: true 
+  },
+  text :{
+    type: String,
+    required: true
+  }
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+Tweet
+  .create({ handle: 'chris', text: 'my first tweet' })
+  .then(createdTweet => console.log(createdTweet));
+
