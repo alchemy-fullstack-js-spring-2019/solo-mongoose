23fded7a328c3a55170102ef71c3853a097f59ee
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-16 11:41:20 -0700

    find

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -1,1 +1,30 @@
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://127.0.0.1:27017/tweets', { 
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const tweetSchema = new mongoose.Schema({
+  handle: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+const Tweet = mongoose.model('Tweet2', tweetSchema);
+
+Tweet
+  .create({ handle: 'another', text: 'this is a tweet' })
+  .then(createdTweet => console.log(createdTweet));
+
+Tweet
+  .find()
+  .then(tweets => console.log(tweets))
+  .finally(() => {
+    mongoose.connection.close();
+  });
