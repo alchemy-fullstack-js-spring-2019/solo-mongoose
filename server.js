19d75c55c4d9cdcc58c8f764df2d5a781161bfa8
Cheri Portman <cheriportman@hotmail.com>
2019-04-16 15:17:06 -0700

    'body requred' element of schema passes test

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require('dotenv').config;
+
 const mongoose = require('mongoose');
 const app = require('./lib/app');
 
-mongoose.connect('mongodb://localhost:27017/tweets', {
+mongoose.connect('mongodb://localhost:27017/tweets', {  //(PROCESS.ENV.mongodb_uri, {......
   useFindAndModify: false,
   useNewUrlParser: true,
   useCreateIndex: true
