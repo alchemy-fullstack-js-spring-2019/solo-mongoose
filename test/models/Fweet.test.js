68dfaca31e48b29a4fbbd7dcb9195752bb53960c
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-16 13:52:07 -0700

    Tweet model fully tested

--- test/models/Fweet.test.js
+++ test/models/Fweet.test.js
@@ -1,18 +1,37 @@
+const mongoose = require('mongoose');
 const Fweet = require('../../lib/models/Fweet');
 
 describe('Fweet model', () => {
   it('creates a fweet with a handle and body', () => {
-    Fweet
-      .create({
-        handle: 'chris',
-        body: 'this is a tweet'
-      })
-      .then(createdTweet => {
-        expect(createdTweet.toJSON()).toEqual({
-          handle: 'chris',
-          body: 'this is a tweet',
-          _id: expect.any(String)
-        });
-      });
+    const fweet = new Fweet({
+      handle: 'chris',
+      body: 'this is a tweet'
+    });
+
+    expect(fweet.toJSON()).toEqual({
+      handle: 'chris',
+      body: 'this is a tweet',
+      _id: expect.any(mongoose.Types.ObjectId)
+    });
   });
+
+  it('has a required handle field', () => {
+    const fweet = new Fweet({
+      body: 'this is a tweet without a handle'
+    });
+
+    const errors = fweet.validateSync().errors;
+
+    expect(errors.handle.message).toBe('Path `handle` is required.');
+  }); 
+
+  it('has a required body field', () => {
+    const fweet = new Fweet({
+      handle: 'nobody'
+    });
+
+    const errors = fweet.validateSync().errors;
+
+    expect(errors.body.message).toBe('Path `body` is required.');
+  }); 
 });
