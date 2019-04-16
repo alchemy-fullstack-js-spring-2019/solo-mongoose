9df8bc2d0dcbf533a15cbdd12d4f43bc540bb3ef
Sean Nelson <smnelsonn@gmail.com>
2019-04-16 11:30:39 -0700

    starting after ryan lecture

---
+++ mongoosePlayground.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/tweets', {
+  useNewUrlParser: true
+});
+
+const tweetSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+// Tweet
+//   .create({ name: 'Sean', text: 'Hello there!' })
+//   .then(createdTweet => console.log(createdTweet));
+
+// Tweet
+//   .findByIdAndUpdate('5cb61af5d0b1eb18fb751792', { name: 'Old2' }, { new: false })
+//   .then (result =>  console.log(result))
+//   .finally(() => { mongoose.connection.close();
+//   });
+
+// Tweet
+//   .findByIdAndDelete('5cb61af5d0b1eb18fb751792')
+//   .then(result => console.log(result))
+//   .finally(() =>  mongoose.connection.close());
+
+Tweet
+  .findById('5cb61ae427209c18dd81b1ba')
+  .then(result => console.log(result))
+  .finally(() => mongoose.connection.close());
