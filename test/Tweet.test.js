ffa729cd3f3409f20d6345d9b03003a62f3564d3
Ben Parisot <me@benparisot.com>
2019-04-16 13:49:36 -0700

    write and pass first Tweet test

--- test/Tweet.test.js
+++ test/Tweet.test.js
@@ -1,2 +1,13 @@
 const Tweet = require('../lib/models/Tweet');
 const mongoose = require('mongoose');
+
+describe('Tweet model test', () => {
+  it('has a handle and a body', () => {
+    const tweet = new Tweet({ handle: 'ben', body: 'hello' });
+    expect(tweet.toJSON()).toEqual({
+      _id: expect.any(mongoose.Types.ObjectId),
+      handle: 'ben',
+      body: 'hello'
+    });
+  });
+});
