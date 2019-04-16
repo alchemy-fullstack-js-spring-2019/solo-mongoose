df17bf686754b43818ebab411b2b21a25abd3220
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-16 14:38:12 -0700

    pre heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./lib/app');
 
-const PORT = 7890;
+const PORT = process.env.PORT || 7890;
 
 mongoose.connect('mongodb://127.0.0.1:27017/fweet', {
   useNewUrlParser: true,
