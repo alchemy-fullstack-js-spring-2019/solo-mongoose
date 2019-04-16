37ae5bb69d227c90b5950eb7278716feabecde88
ollinieminen <otn987@gmail.com>
2019-04-16 14:04:06 -0700

    add app.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
+const app = require('./lib/app');
 
 mongoose.connect('mongodb://localhost:27017/tweets', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true
 });
+
+app.listen(7890, ()=>{
+  console.log('listening to port 7890');
+});
+
+
+
