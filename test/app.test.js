8d2875ad26969f128cbbbbce4fdab01a24187f23
Cara Ponzini <ponzini@gmail.com>
2019-04-16 15:21:29 -0700

    created and passed tests for users through patch

--- test/app.test.js
+++ test/app.test.js
@@ -37,55 +37,56 @@ describe('user routes', () => {
       });
   });
 
-  // it('can get all the tweets', () => {
-  //   return Tweet
-  //     .create({ handle: 'cara', text: 'I am a tweet' })
-  //     .then(() => {
-  //       return request(app)
-  //         .get('/tweets');
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toHaveLength(1);
-  //     });
-  // });
+  it('can get all the users', () => {
+    return User
+      .create({ handle: 'cara_handler', name: 'Iam A Person', email: 'email@internets.com' })
+      .then(() => {
+        return request(app)
+          .get('/users');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
 
-  // it('can get a tweet by id', () => {
-  //   return Tweet
-  //     .create({ handle: 'cara', text: 'here is some text' })
-  //     .then(newTweet => {
-  //       return request(app)
-  //         .get(`/tweets/${newTweet._id}`);
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({ 
-  //         handle: 'cara', 
-  //         text: 'here is some text', 
-  //         _id: expect.any(String),
-  //         __v: 0 
-  //       });
-  //     });
-  // });
+  it('can get a user by id', () => {
+    return User
+      .create({ handle: 'new_handle', name: 'New Person', email: 'email@internets.com' })
+      .then(newUser => {
+        return request(app)
+          .get(`/users/${newUser._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          handle: 'new_handle', 
+          name: 'New Person', 
+          email: 'email@internets.com', 
+          _id: expect.any(String),
+          __v: 0 
+        });
+      });
+  });
 
-  // it('can find a tweet by id and update', () => {
-  //   return Tweet
-  //     .create({ handle: 'cara', text: 'here is some text' })
-  //     .then(newTweet => {
-  //       return request(app)
-  //         .patch(`/tweets/${newTweet._id}`)
-  //         .send({
-  //           handle: 'cara',
-  //           text: 'new text new text'
-  //         });
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({ 
-  //         handle: 'cara', 
-  //         text: 'new text new text', 
-  //         _id: expect.any(String),
-  //         __v: 0 
-  //       });
-  //     });
-  // });
+  it('can find a user by id and update', () => {
+    return User
+      .create({ handle: 'aspasp', name: 'Moses', email: 'moses@email.com' })
+      .then(newUser => {
+        return request(app)
+          .patch(`/users/${newUser._id}`)
+          .send({
+            email: 'NewEmail@place.com'
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          handle: 'aspasp', 
+          name: 'Moses', 
+          email: 'NewEmail@place.com', 
+          _id: expect.any(String),
+          __v: 0 
+        });
+      });
+  });
 
   // it('can delete a tweet by id', () => {
   //   const aTweet = { handle: 'cara', text: 'here is some text' };
