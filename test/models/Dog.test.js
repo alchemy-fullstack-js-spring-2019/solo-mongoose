4354bfeb449a7a517c24e2d4f05f4a5467a48b89
Payton Lower <paytown@me.com>
2019-04-16 13:47:55 -0700

    name and age required

--- test/models/Dog.test.js
+++ test/models/Dog.test.js
@@ -7,11 +7,26 @@ describe('Dogs', () => {
       name: 'Tommy',
       age: 12
     });
-    
     expect(dog.toJSON()).toEqual({
       name: 'Tommy',
       age: 12,
       _id: expect.any(mongoose.Types.ObjectId)
     });
+  });
+
+  it('requires name', () => {
+    const dog = new Dog({
+      age: 12
+    });
+    const errors = dog.validateSync().errors;
+    expect(errors.name.message).toEqual('Path `name` is required.');
+  });
+
+  it('requires age', () => {
+    const dog = new Dog({
+      name: 'Todd'
+    });
+    const errors = dog.validateSync().errors;
+    expect(errors.age.message).toEqual('Path `age` is required.');
   });
 });
