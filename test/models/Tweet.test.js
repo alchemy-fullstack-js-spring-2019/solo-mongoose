3e7cbd679dd07dbdb55eafc894fb89b6ae4ddf07
Tommy Tran <tranttommy95@gmail.com>
2019-04-16 14:23:30 -0700

    Tweet model tests passing

--- test/models/Tweet.test.js
+++ test/models/Tweet.test.js
@@ -10,12 +10,32 @@ describe('tweetSchema tests', () => {
       tags: [],
       _id: expect.any(mongoose.Types.ObjectId)
     };
-    const input = {
+    const tweet = new Tweet({
       handle: 'Tommy',
       body: 'tweet4'
-    };
-    const result = new Tweet(input);
+    });
+    
+    const error = tweet.validateSync();
+    
+    expect(error).toBeFalsy();
+    expect(tweet.toJSON()).toEqual(expected);
+  });
 
-    expect(result.toJSON()).toEqual(expected);
+  it('requires a handle', () => {
+    const tweet = new Tweet({
+      body: 'tweet4'
+    });
+    const error = tweet.validateSync();
+    expect(error).toBeTruthy();
+  });
+
+  it('limits characters (140)', () => {
+    const tweet = new Tweet({
+      handle: 'Tommy',
+      body: '@'.repeat(141)
+    });
+  
+    const error = tweet.validateSync();
+    expect(error).toBeTruthy();
   });
 });
