c5e569c2a97ad7193af2d5d569d117e7915b467d
Parker Helland <hellandp@gmail.com>
2019-04-16 14:17:20 -0700

    NINJA MODELS TESTS, has required fields passed

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ mongoose.connect('mongodb://localhost:27017/ninjas', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true
-});
+}, console.log('√ Server Started √'));
---
+++ test/models/Ninja.test.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Ninja = require('../../lib/models/Ninja');
+
+describe('NINJA MODELS TESTS', () => {
+
+  it('has nickname, age, and tagline', () => {
+    const ninja = new Ninja({
+      nickname: 'Shadow',
+      age: 23,
+      tagline: 'One with the night'
+    });
+
+    expect(ninja.toJSON()).toEqual({
+      nickname: 'Shadow',
+      age: 23,
+      tagline: 'One with the night',
+      _id: expect.any(mongoose.Types.ObjectId)
+    });
+  });
+
+});
