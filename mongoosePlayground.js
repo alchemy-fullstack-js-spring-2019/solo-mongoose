2c3f5fdc090ffd9d91db0dd53a24c1bb27a4457d
Payton Lower <paytown@me.com>
2019-04-16 12:03:19 -0700

    tweetin

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -15,7 +15,33 @@ const tweetSchema = new mongoose.Schema({
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 
+// Tweet
+// .create({ handle: 'imawaterbottle', text: 'covfefe' })
+// .then(createdTweet => console.log(createdTweet))
+// .finally(() => mongoose.connection.close());
+  
+// Tweet
+//   .find()
+//   .then(tweets => console.log(tweets))
+//   .finally(() => mongoose.connection.close());
+
+// Tweet
+//   .create({ handle: 'ted', text: 'who am i' })
+//   .then(createdTweet => {
+//     return Tweet.findById(createdTweet._id);
+//   })
+//   .then(foundTweet => console.log(foundTweet))
+//   .finally(() => mongoose.connection.close());
+
+// Tweet
+//   .findById('5cb624bd37c4c06115fbcae6')
+//   .then(foundTweet => console.log(foundTweet))
+//   .finally(() => mongoose.connection.close());
+
+// Tweet
+//   .findByIdAndUpdate('5cb624bd37c4c06115fbcae6', { text: 'senor. covfefe' }, { new: true })
+//   .then(updatedTweet => console.log(updatedTweet))
+//   .finally(() => mongoose.connection.close());
+
 Tweet
-  .create({ handle: 'imadog', text: 'woof woof woof' })
-  .then(createdTweet => console.log(createdTweet))
-  .finally(() => mongoose.connection.close());
+  .findByIdAndDelete('5cb624afee7b346107250594')
