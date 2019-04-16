98131c7a29e8bc986b5f064b85dbeebad531786f
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-16 13:44:38 -0700

    server and app setup

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const app = require('./lib/app');
+
+const PORT = 7890;
 
 mongoose.connect('mongodb://127.0.0.1:27017/fweet', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
