8fa3acd07006c67b1a49870630d3581af437347a
Emily Creamer <emily.creamer@icloud.com>
2019-04-16 11:51:41 -0700

    created tweet

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -1,4 +1,23 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/tweets');
+mongoose.connect('mongodb://localhost:27017/tweets', {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
+const tweetSchema = new mongoose.Schema({
+  handle: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+Tweet.create({ handle: 'emily', text: 'first tweet' })
+  .then(createdTweet => console.log(createdTweet));
+  // .finally(() => mongoose.connections.close());
