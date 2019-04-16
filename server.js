3c967184287098408bea991096fa3fd25eae5315
Cara Ponzini <ponzini@gmail.com>
2019-04-16 16:08:12 -0700

    added dotenv and heroku setup

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+const app = require('./lib/app');
+const PORT = process.env.PORT || 7890;
 
-mongoose.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+  console.log(`running on ${PORT}`);
+});
+
