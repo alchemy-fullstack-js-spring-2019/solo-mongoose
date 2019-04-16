b33a29f039cdcdfc1d62d9b91de538f979ad420b
Cara Ponzini <ponzini@gmail.com>
2019-04-16 15:07:37 -0700

    updating tests for users

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../lib/app');
-const Tweet = require('../lib/models/Tweet');
+const User = require('../lib/models/User');
 
-describe('tweet routes', () => {
+describe('user routes', () => {
   beforeAll(() => {
-    return mongoose.connect('mongodb://localhost:27017/tweets', { 
+    return mongoose.connect('mongodb://localhost:27017/users', { 
       useFindAndModify: false,  
       useNewUrlParser: true,
       useCreateIndex: true 
@@ -18,88 +18,90 @@ describe('tweet routes', () => {
     return mongoose.connection.close();
   });
 
-  it('can create a new tweet', () => {
+  it('can create a new user', () => {
     return request(app)
-      .post('/tweets')
+      .post('/users')
       .send({
-        handle: 'cara',
-        text: 'tweets have text'
+        handle: 'cara_handle',
+        name: 'cara',
+        email: 'email@email.com'
       })
       .then(res => {
         expect(res.body).toEqual({
-          handle: 'cara',
-          text: 'tweets have text',
+          handle: 'cara_handle',
+          name: 'cara',
+          email: 'email@email.com',
           _id: expect.any(String),
           __v: 0
         });
       });
   });
 
-  it('can get all the tweets', () => {
-    return Tweet
-      .create({ handle: 'cara', text: 'I am a tweet' })
-      .then(() => {
-        return request(app)
-          .get('/tweets');
-      })
-      .then(res => {
-        expect(res.body).toHaveLength(1);
-      });
-  });
+  // it('can get all the tweets', () => {
+  //   return Tweet
+  //     .create({ handle: 'cara', text: 'I am a tweet' })
+  //     .then(() => {
+  //       return request(app)
+  //         .get('/tweets');
+  //     })
+  //     .then(res => {
+  //       expect(res.body).toHaveLength(1);
+  //     });
+  // });
 
-  it('can get a tweet by id', () => {
-    return Tweet
-      .create({ handle: 'cara', text: 'here is some text' })
-      .then(newTweet => {
-        return request(app)
-          .get(`/tweets/${newTweet._id}`);
-      })
-      .then(res => {
-        expect(res.body).toEqual({ 
-          handle: 'cara', 
-          text: 'here is some text', 
-          _id: expect.any(String),
-          __v: 0 
-        });
-      });
-  });
+  // it('can get a tweet by id', () => {
+  //   return Tweet
+  //     .create({ handle: 'cara', text: 'here is some text' })
+  //     .then(newTweet => {
+  //       return request(app)
+  //         .get(`/tweets/${newTweet._id}`);
+  //     })
+  //     .then(res => {
+  //       expect(res.body).toEqual({ 
+  //         handle: 'cara', 
+  //         text: 'here is some text', 
+  //         _id: expect.any(String),
+  //         __v: 0 
+  //       });
+  //     });
+  // });
 
-  it('can find a tweet by id and update', () => {
-    return Tweet
-      .create({ handle: 'cara', text: 'here is some text' })
-      .then(newTweet => {
-        return request(app)
-          .patch(`/tweets/${newTweet._id}`)
-          .send({
-            handle: 'cara',
-            text: 'new text new text'
-          });
-      })
-      .then(res => {
-        expect(res.body).toEqual({ 
-          handle: 'cara', 
-          text: 'new text new text', 
-          _id: expect.any(String),
-          __v: 0 
-        });
-      });
-  });
+  // it('can find a tweet by id and update', () => {
+  //   return Tweet
+  //     .create({ handle: 'cara', text: 'here is some text' })
+  //     .then(newTweet => {
+  //       return request(app)
+  //         .patch(`/tweets/${newTweet._id}`)
+  //         .send({
+  //           handle: 'cara',
+  //           text: 'new text new text'
+  //         });
+  //     })
+  //     .then(res => {
+  //       expect(res.body).toEqual({ 
+  //         handle: 'cara', 
+  //         text: 'new text new text', 
+  //         _id: expect.any(String),
+  //         __v: 0 
+  //       });
+  //     });
+  // });
 
-  it('can delete a tweet by id', () => {
-    const aTweet = { handle: 'cara', text: 'here is some text' };
-    return Tweet
-      .create(aTweet)
-      .then(newTweet => {
-        return request(app)
-          .delete(`/tweets/${newTweet._id}`);
-      })
-      .then(res => {
-        expect(res.body).toEqual({ 
-          handle: 'cara', 
-          text: 'here is some text', 
-          _id: expect.any(String),
-          __v: 0 
-        });
-      });
-  });
+  // it('can delete a tweet by id', () => {
+  //   const aTweet = { handle: 'cara', text: 'here is some text' };
+  //   return Tweet
+  //     .create(aTweet)
+  //     .then(newTweet => {
+  //       return request(app)
+  //         .delete(`/tweets/${newTweet._id}`);
+  //     })
+  //     .then(res => {
+  //       expect(res.body).toEqual({ 
+  //         handle: 'cara', 
+  //         text: 'here is some text', 
+  //         _id: expect.any(String),
+  //         __v: 0 
+  //       });
+  //     });
+  // });
 });
