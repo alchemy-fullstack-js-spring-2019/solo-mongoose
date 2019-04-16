5a16e0e688c37e9d2ea3110375c76f502b4b8283
Sean Nelson <smnelsonn@gmail.com>
2019-04-16 11:39:56 -0700

    create>findsame>log

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -17,17 +17,27 @@ const tweetSchema = new mongoose.Schema({
 });
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
-
+//create
+// Tweet
+//   .create({ name: 'Sean', text: 'Hello there!' })
+//   .then(createdTweet => console.log(createdTweet));
+// //findByID
+// Tweet
+//   .findById('5cb61ae427209c18dd81b1ba')
+//   .then(result => console.log(result));
+//   //Create then find
 Tweet
   .create({ name: 'Sean', text: 'Hello there!' })
-  .then(createdTweet => console.log(createdTweet));
-Tweet
-  .findById('5cb61ae427209c18dd81b1ba')
-  .then(result => console.log(result))
+  .then(created => {
+    return Tweet.findById(created._id);
+  })
+  .then(found => console.log(found))
   .finally(() => mongoose.connection.close());
 
+
+
 // Tweet
-//   .findByIdAndUpdate('5cb61af5d0b1eb18fb751792', { name: 'Old2' }, { new: false })
+//   .findByIdAndUpdate('5cb6211fc9a0671bf4a2c9f6', { text: 'WoW' }, { new: true })
 //   .then (result =>  console.log(result))
 //   .finally(() => { mongoose.connection.close();
 //   });
