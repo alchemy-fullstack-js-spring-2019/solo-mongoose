9d5c28a46a214964afb84e52f69524886c673e8b
Sean Nelson <smnelsonn@gmail.com>
2019-04-16 11:44:47 -0700

    update

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/tweets', {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
 const tweetSchema = new mongoose.Schema({
@@ -26,21 +27,20 @@ const Tweet = mongoose.model('Tweet', tweetSchema);
 //   .findById('5cb61ae427209c18dd81b1ba')
 //   .then(result => console.log(result));
 //   //Create then find
-Tweet
-  .create({ name: 'Sean', text: 'Hello there!' })
-  .then(created => {
-    return Tweet.findById(created._id);
-  })
-  .then(found => console.log(found))
-  .finally(() => mongoose.connection.close());
-
-
-
 // Tweet
-//   .findByIdAndUpdate('5cb6211fc9a0671bf4a2c9f6', { text: 'WoW' }, { new: true })
-//   .then (result =>  console.log(result))
-//   .finally(() => { mongoose.connection.close();
-//   });
+//   .create({ name: 'Sean', text: 'Hello there!' })
+//   .then(created => {
+//     return Tweet.findById(created._id);
+//   })
+//   .then(found => console.log(found));
+
+
+
+Tweet
+  .findByIdAndUpdate('5cb6211fc9a0671bf4a2c9f6', { text: 'WoW' }, { new: true })
+  .then (result =>  console.log(result))
+  .finally(() => { mongoose.connection.close();
+  });
 
 // Tweet
 //   .findByIdAndDelete('5cb61af5d0b1eb18fb751792')
