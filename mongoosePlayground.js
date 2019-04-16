d23ca07318351d2737bbb2122d6563e93f7a46f2
Payton Lower <paytown@me.com>
2019-04-16 11:50:50 -0700

    mongoose tweet working

---
+++ mongoosePlayground.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/tweets', { useNewUrlParser: true });
+
+const tweetSchema = new mongoose.Schema({
+  handle: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+Tweet
+  .create({ handle: 'imadog', text: 'woof woof woof' })
+  .then(createdTweet => console.log(createdTweet));
