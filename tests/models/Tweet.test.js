eee3108f54e3dd88971aed705d4900004f655e6d
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-17 14:03:34 -0700

    refactor Tweet model test for rels

--- tests/models/Tweet.test.js
+++ tests/models/Tweet.test.js
@@ -1,25 +1,31 @@
 const Tweet = require('../../lib/models/Tweet');
 const mongoose = require('mongoose');
+const Dog = require('../../lib/models/Dog');
 
 describe('tweet model', () => {
-  it('has a handle and body field', () => {
+  it('has a user and body field', () => {
+    const dog = new Dog({
+      name: 'rover',
+      handle: 'rovedog77',
+      email: 'rover@rovedog.com'
+    });
     const tweet = new Tweet({
-      handle: 'meg',
+      user: dog._id,
       body: 'tweeting is so fun'
     });
     expect(tweet.toJSON()).toEqual({
-      handle: 'meg',
+      user: dog._id,
       body: 'tweeting is so fun',
       _id: expect.any(mongoose.Types.ObjectId)
     });
   });
 
-  it('has a required handle field', () => {
+  it('has a required user field', () => {
     const tweet = new Tweet({
       body: 'I can\'t even tell you how much I like tweeting'
     });
     const errors = tweet.validateSync().errors;
-    expect(errors.handle.message).toEqual('Path `handle` is required.');
+    expect(errors.user.message).toEqual('Path `user` is required.');
   });
 
   it('has a required body field', () => {
