08576b3e84cbc00b25730a3c944dbaab805d170c
Mallarie <mallariemilano@gmail.com>
2019-04-17 10:38:21 -0700

    added const port for heroku

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const app = require('./lib/app');
 
+const PORT = process.env.PORT || 7890;
+
 mongoose.connect('mongodb://localhost:27017/tweets', { 
   useFindAndModify: false,
   useNewUrlParser: true,
   useCreateIndex: true  
 });
 
-app.listen(7890, () => {
-  console.log('Started on port 7890');
+app.listen(PORT, () => {
+  console.log(`Started on port ${PORT}`);
 });
