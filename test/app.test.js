3e08f03c30b699b3baa35f5549e1f065353e91eb
Parker Helland <hellandp@gmail.com>
2019-04-16 22:17:39 -0700

    post testing works! ROBO3T stored data also

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,30 @@
-describe('', () => {
+const mongoose = require('mongoose');
+const app = require('../lib/app');
+const request = require('supertest');
+// const Ninja = require('../lib/models/Ninja');
+require('dotenv').config();
 
-  it('', () => {
-    
+describe('APP TESTS', () => {
+  beforeAll(() => {
+    return mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    });
+  });
+  
+  it('creates a new ninja', () => {
+    return request(app)
+      .post('/ninjas')
+      .send({ nickname: 'nino', age: 20, tagline: 'death' })
+      .then(res => {
+        expect(res.body).toEqual({
+          nickname: 'nino', 
+          age: 20, 
+          tagline: 'death',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
   });
 });
