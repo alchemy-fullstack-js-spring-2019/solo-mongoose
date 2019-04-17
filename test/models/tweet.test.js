077a2cdaec8878df150702c34d0db86a48bba759
dreamer <wildetangeant@gmail.com>
2019-04-17 13:29:52 -0700

    unit tests built

--- test/models/tweet.test.js
+++ test/models/tweet.test.js
@@ -7,7 +7,6 @@ describe('Tweet unit test', () => {
       handle: 'vapid viper',
       body: 'The shadows of the abyss are like the petals of a monstrous flower that shall blossom within the skull'
     });
-
     expect(tweet.toJSON()).toEqual({
       handle: 'vapid viper',
       body: 'The shadows of the abyss are like the petals of a monstrous flower that shall blossom within the skull',
@@ -15,4 +14,25 @@ describe('Tweet unit test', () => {
     });
   });
 
+  it('has required body', () => {
+    const tweet = new Tweet({
+      handle: 'zealous zebra',
+    });
+
+    const errors = tweet.validateSync().errors;
+  
+    expect(errors.body.message).toEqual('Path `body` is required.');
+  });
+
+  it('has required handle', () => {
+    const tweet = new Tweet({
+      body: 'Where lies the strangling fruit that came from the hand of the sinner I shall bring forth the seeds of tnpmhe dead to share with the worms'
+    });
+
+    const errors = tweet.validateSync().errors;
+  
+    expect(errors.handle.message).toEqual('Path `handle` is required.');
+  });
+
 });
+
