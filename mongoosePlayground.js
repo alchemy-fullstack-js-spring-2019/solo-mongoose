ef84e7a3058756b41be326a24fd1f421b09bf2b7
Cara Ponzini <ponzini@gmail.com>
2019-04-17 13:47:30 -0700

    changed id test for tweet

--- mongoosePlayground.js
+++ mongoosePlayground.js
@@ -7,27 +7,19 @@ mongoose.connect('mongodb://localhost:27017/tweets', {
   useFindAndModify: false
 });
 
-
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
   handle: {
     type: String,
     required: true
   },
-  email: {
-    type: String,
-    required: true,
-    match: /\w*@\w*\.\w*/g
+  image: {
+    type: String
   }
 });
-
 const User = mongoose.model('User', userSchema);
 
 const tweetSchema = new mongoose.Schema({
-  handle: {
+  user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User', //User with no string?
     required: true
@@ -40,52 +32,74 @@ const tweetSchema = new mongoose.Schema({
   },
   tag: {
     type: String, //array of strings --> [String]
-    required: true,
+    // required: true,
     enum: ['person', 'dog', 'cactus', 'random'] //only allows certain tags
   },
   followers:  {
     type: Number,
-    required: true,
+    // required: true,
     default: 0,
     min: 0
-  },
-  address: {
-    street: String,
-    city: String,
-    state: String // these could each be their own thing...
   }
 });
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 
-// create and find by ID
-Tweet
-  .create({ handle: 'cara', text: 'tweet one' })
-  .then(createdTweet => {
-    return Tweet.findById(createdTweet._id);
-  })
-  .then(foundTweet => console.log(foundTweet))
-  .finally(() => mongoose.connection.close());
 
-//Create, find by ID and delete
-Tweet
-  .create({ handle: 'cara', text: 'I love cactus', tag: 'cactus' })
-  .then(newTweet => {
-    return Tweet.findByIdAndDelete(newTweet._id);
-  })
-  .then(res => console.log(res))
-  .finally(() => mongoose.connection.close());
+// User
+// .create({
+//   handle: 'cara',
+//   image: 'image url'
+// })
+// .then(createdUser => {
+//   return Tweet.create({
+//     user: createdUser._id,
+//     text: 'here is a tweet'
+//   })
+// })
+// .then(tweet => console.log(tweet))
+// .finally(() => mongoose.connection.close())
 
-//create, find by ID and update
 Tweet
-  .create({ handle: 'cara', text: 'I love cactus', tag: 'cactus' })
-  .then(newTweet => {
-    return Tweet.findByIdAndUpdate(newTweet._id, { text: 'I love mice' }, { new: true });
-  })
-  .then(res => console.log(res))
-  .finally(() => mongoose.connection.close());
+.findById('5cb78b348fc75c4bc9f4fba3')
+.populate('user', 'handle')
+.select({
+  __v: false,
+})
+.lean()
+.then(console.log)
+.finally(() => mongoose.connection.close());
 
-//find all tweets
-Tweet.find()
-  .then(tweets => console.log(tweets))
-  .finally(() => mongoose.connection.close());
+
+
+// // create and find by ID
+// Tweet
+//   .create({ handle: 'cara', text: 'tweet one' })
+//   .then(createdTweet => {
+//     return Tweet.findById(createdTweet._id);
+//   })
+//   .then(foundTweet => console.log(foundTweet))
+//   .finally(() => mongoose.connection.close());
+
+// //Create, find by ID and delete
+// Tweet
+//   .create({ handle: 'cara', text: 'I love cactus', tag: 'cactus' })
+//   .then(newTweet => {
+//     return Tweet.findByIdAndDelete(newTweet._id);
+//   })
+//   .then(res => console.log(res))
+//   .finally(() => mongoose.connection.close());
+
+// //create, find by ID and update
+// Tweet
+//   .create({ handle: 'cara', text: 'I love cactus', tag: 'cactus' })
+//   .then(newTweet => {
+//     return Tweet.findByIdAndUpdate(newTweet._id, { text: 'I love mice' }, { new: true });
+//   })
+//   .then(res => console.log(res))
+//   .finally(() => mongoose.connection.close());
+
+// //find all tweets
+// Tweet.find()
+//   .then(tweets => console.log(tweets))
+//   .finally(() => mongoose.connection.close());
--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,115 @@ const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../lib/app');
 const User = require('../lib/models/User');
+const Tweet = require('../lib/models/Tweet');
+
+describe('tweet routes', () => {
+  beforeAll(() => {
+    return mongoose.connect('mongodb://localhost:27017/tweets', { 
+      useFindAndModify: false,  
+      useNewUrlParser: true,
+      useCreateIndex: true 
+    });
+  });
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can create a new tweet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        handle: 'cara',
+        text: 'tweets have text'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'cara',
+          text: 'tweets have text',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('can get all the tweets', () => {
+    return Tweet
+      .create({ handle: 'cara', text: 'I am a tweet' })
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
+
+  it.only('can get a tweet by id', () => {
+    User
+      .create({
+        handle: 'cara',
+        image: 'image url'
+      })
+      .then(createdUser => {
+        return Tweet.create({
+          user: createdUser._id,
+          text: 'here is some text'
+        });
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          user: {
+            _id: expect.any(String),
+            handle: 'cara',
+          }, 
+          text: 'here is some text', 
+          _id: expect.any(String),
+        });
+      });
+  });
+
+  it('can find a tweet by id and update', () => {
+    return Tweet
+      .create({ handle: 'cara', text: 'here is some text' })
+      .then(newTweet => {
+        return request(app)
+          .patch(`/tweets/${newTweet._id}`)
+          .send({
+            handle: 'cara',
+            text: 'new text new text'
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          handle: 'cara', 
+          text: 'new text new text', 
+          _id: expect.any(String),
+          __v: 0 
+        });
+      });
+  });
+
+  it('can delete a tweet by id', () => {
+    const aTweet = { handle: 'cara', text: 'here is some text' };
+    return Tweet
+      .create(aTweet)
+      .then(newTweet => {
+        return request(app)
+          .delete(`/tweets/${newTweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          handle: 'cara', 
+          text: 'here is some text', 
+          _id: expect.any(String),
+          __v: 0 
+        });
+      });
+  });
+});
 
 describe('user routes', () => {
   beforeAll(() => {
