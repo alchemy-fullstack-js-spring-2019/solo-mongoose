2560f1399e2eec625b6da6bdff749dca4ad6c085
dreamer <wildetangeant@gmail.com>
2019-04-16 17:27:07 -0700

    return a list of users test passing (e2e)

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
 
 describe('e2e user routes', () => {
   beforeAll(() => {
-    return mongoose.connect('mongodb://localhost:27107/users', {
+    return mongoose.connect('mongodb://localhost:27017/users', {
       useFindAndModify: false,
       useNewUrlParser: true,
       useCreateIndex: true
@@ -19,20 +20,36 @@ describe('e2e user routes', () => {
     return mongoose.connection.close();
   });
 
-  // it('can create User', () => {
-  //   return request(app)
-  //     .post('/users')
-  //     .send({
-  //       handle: 'runner',
-  //       name: 'reckless_escapism'
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         handle: 'runner',
-  //         name: 'reckless_escapism'
-  //       });
-  //     });
-  // });
+  it('can create User', () => {
+    return request(app)
+      .post('/users')
+      .send({
+        handle: 'runner',
+        name: 'reckless_escapism'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'runner',
+          name: 'reckless_escapism',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('return a list of users', () => {
+    return User
+      .create({ 
+        handle: 'curiouscoyote', 
+        name: 'erroneous error' })
+      .then(() => {
+        return request(app)
+          .get('/users');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
 
 });
 
