5829f29a52dd56ea29335f03e361c6a01efada5f
UncleJerry <jared.myhrberg@gmail.com>
2019-04-17 08:55:16 -0700

    commented out playground

--- mongoodPlayground.js
+++ mongoodPlayground.js
@@ -1,48 +1,48 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/tweets', {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect('mongodb://localhost:27017/tweets', {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
-const tweetsSchem = new mongoose.Schema({
-  handle: {
-    type: String,
-    required: true
-  },
-  body: {
-    type: String,
-    required: true
-  }
-});
-const Tweet = mongoose.model('Tweet', tweetsSchem);
+// const tweetsSchema = new mongoose.Schema({
+//   handle: {
+//     type: String,
+//     required: true
+//   },
+//   body: {
+//     type: String,
+//     required: true
+//   }
+// });
+// const Tweet = mongoose.model('Tweet', tweetsSchema);
 
-Tweet
-  .create({ handle: 'sven', body: 'my first tweet' })
-  .then(createdTweet => {
-    return Tweet.findById(createdTweet._id);
-  })
-  .then(foundTweet => console.log(foundTweet));
+// Tweet
+//   .create({ handle: 'sven', body: 'my first tweet' })
+//   .then(createdTweet => {
+//     return Tweet.findById(createdTweet._id);
+//   })
+//   .then(foundTweet => console.log(foundTweet));
 
-Tweet
-  .find()
-  .then(tweets => console.log(tweets));
+// Tweet
+//   .find()
+//   .then(tweets => console.log(tweets));
 
-Tweet
-  .create({ handle: 'sven', body: 'my first tweet' })
-  .then(createdTweet => {
-    return Tweet.findById(createdTweet._id);
-  })
-  .then(foundTweet => console.log(foundTweet));
+// Tweet
+//   .create({ handle: 'sven', body: 'my first tweet' })
+//   .then(createdTweet => {
+//     return Tweet.findById(createdTweet._id);
+//   })
+//   .then(foundTweet => console.log(foundTweet));
 
-Tweet .create({ handle: 'sven', body: 'my first tweet' })
-  .then(createdTweet => {
-    return Tweet.findByIdAndUpdate(createdTweet._id, { body: 'hey there ho there' }, { new: true });
-  })
-  .then(updatedTweet => console.log(updatedTweet));
+// Tweet .create({ handle: 'sven', body: 'my first tweet' })
+//   .then(createdTweet => {
+//     return Tweet.findByIdAndUpdate(createdTweet._id, { body: 'hey there ho there' }, { new: true });
+//   })
+//   .then(updatedTweet => console.log(updatedTweet));
 
-Tweet
-  .findByIdAndDelete('5cb623014b7084418c1eb070')
-  .then(deleted => console.log(deleted));
+// Tweet
+//   .findByIdAndDelete('5cb623014b7084418c1eb070')
+//   .then(deleted => console.log(deleted));
 
 
