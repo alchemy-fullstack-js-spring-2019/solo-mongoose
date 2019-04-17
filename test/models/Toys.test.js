a63c94cc8f4a9c1526eda0f1dfbbf842eabe998a
Cheri Portman <cheriportman@hotmail.com>
2019-04-16 20:00:49 -0700

    basic stats of toys schema passes test

--- test/models/Toys.test.js
+++ test/models/Toys.test.js
@@ -1,1 +1,22 @@
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
+const Toy = require('../../lib/models/Toys');
+
+describe('Toys model', () => {
+  it('has name, description, color, and condition fields', () => {
+    const toy = new Toy({
+      name: 'the pickle', 
+      description: 'fuzzy pickle',
+      color: 'green',
+      condition: 'squeaker in critical condition'
+    });
+    
+    expect(toy.toJSON()).toEqual({
+      name: 'the pickle', 
+      description: 'fuzzy pickle',
+      color: 'green',
+      condition: 'squeaker in critical condition',
+      _id: expect.any(mongoose.Types.ObjectId)
+    });
+  });
+});
+
