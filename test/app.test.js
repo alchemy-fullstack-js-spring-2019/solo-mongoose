e25d6f5495c7908c715e3e423237c00a94308e0c
Mallarie <mallariemilano@gmail.com>
2019-04-17 08:56:58 -0700

    app test

---
+++ test/app.test.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('tweet routes', () => {
+  beforeAll(() => {
+    return mongoose.connect('mongodb://localhost:27107/tweets', {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
+  });
+});
