dcdad9cf3a701f43018468010640d5706c929423
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-16 17:52:27 -0700

    delete method test passing + non required post test

--- test/app.test.js
+++ test/app.test.js
@@ -33,6 +33,22 @@ describe('Pirate routes', () => {
         });
       });
   });
+  it('creates a pirate document WITHOUT a non-required parameter POST', () => {
+    return request(app)
+      .post('/pirates')
+      .send({
+        name: 'Lila Bard',
+        ship: 'A ship'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'Lila Bard',
+          ship: 'A ship',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
   it('retrieve all docs with GET', () => {
     return Pirate.create({ name: 'King K Rull', ship:'The Jolly Roger', pet:'Donkey Kong' })
       .then(() => {
@@ -58,8 +74,7 @@ describe('Pirate routes', () => {
       });
   });
   it('updates Pirate document when PATCH called', () => {
-    return Pirate
-      .create({name: 'Lila Bard', ship: 'Black Spire'})
+    return Pirate.create({name: 'Lila Bard', ship: 'Black Spire'})
       .then(createdPirate => {
         return request(app)
           .patch(`/pirates/${createdPirate._id}`)
@@ -69,6 +84,21 @@ describe('Pirate routes', () => {
         expect(res.body).toEqual({
           name: 'Lila Bard',
           ship: 'The Sarrows',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+  it('removes a document when DELETE called', () => {
+    return Pirate.create({ name: 'Burly the Boar', ship: 'Riddled with Holes'})
+      .then(createdPirate => {
+        return request(app)
+          .delete(`/pirates/${createdPirate._id}`)
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'Burly the Boar',
+          ship: 'Riddled with Holes',
           _id: expect.any(String),
           __v: 0
         });
