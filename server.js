69e75235bc6fa23f467cc7ebc2b4f5f46b64696b
Tommy Tran <tranttommy95@gmail.com>
2019-04-17 09:27:46 -0700

    trying to get 2 routes to work

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 2309;
 
-mongoose.connect(`${process.env.MONGODB_URI}/tweets`, { useNewUrlParser: true });
-mongoose.connect(`${process.env.MONGODB_URI}/users`, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`);
