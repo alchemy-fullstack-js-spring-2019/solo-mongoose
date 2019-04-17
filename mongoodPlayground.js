e895f659da7467991e422c6172d0f57320c8964f
UncleJerry <jared.myhrberg@gmail.com>
2019-04-16 21:27:50 -0700

    cleaning up

--- mongoodPlayground.js
+++ mongoodPlayground.js
@@ -17,30 +17,29 @@ const tweetsSchem = new mongoose.Schema({
 });
 const Tweet = mongoose.model('Tweet', tweetsSchem);
 
-// Tweet
-//   .create({ handle: 'sven', body: 'my first tweet' })
-//   .then(createdTweet => {
-//     return Tweet.findById(createdTweet._id);
-//   })
-//   .then(foundTweet => console.log(foundTweet));
+Tweet
+  .create({ handle: 'sven', body: 'my first tweet' })
+  .then(createdTweet => {
+    return Tweet.findById(createdTweet._id);
+  })
+  .then(foundTweet => console.log(foundTweet));
 
-// Tweet
-//   .find()
-//   .then(tweets => console.log(tweets));
+Tweet
+  .find()
+  .then(tweets => console.log(tweets));
 
-// Tweet
-//   .create({ handle: 'sven', body: 'my first tweet' })
-//   .then(createdTweet => {
-//     return Tweet.findById(createdTweet._id);
-//   })
-//   .then(foundTweet => console.log(foundTweet));
+Tweet
+  .create({ handle: 'sven', body: 'my first tweet' })
+  .then(createdTweet => {
+    return Tweet.findById(createdTweet._id);
+  })
+  .then(foundTweet => console.log(foundTweet));
 
-// Tweet .create({ handle: 'sven', body: 'my first tweet' })
-//   .then(createdTweet => {
-//     return Tweet.findByIdAndUpdate(createdTweet._id, { body: 'hey there ho there' }, { new: true });
-//   })
-//   .then(updatedTweet => console.log(updatedTweet));
-
+Tweet .create({ handle: 'sven', body: 'my first tweet' })
+  .then(createdTweet => {
+    return Tweet.findByIdAndUpdate(createdTweet._id, { body: 'hey there ho there' }, { new: true });
+  })
+  .then(updatedTweet => console.log(updatedTweet));
 
 Tweet
   .findByIdAndDelete('5cb623014b7084418c1eb070')
