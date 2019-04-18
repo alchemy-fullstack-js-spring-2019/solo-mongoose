e86142c5044f5c20dda37f19d5172c77cefe2410
Masmics <cherlocker3113@gmail.com>
2019-04-18 09:27:49 -0700

    find by id and update added

--- test/app.test.js
+++ test/app.test.js
@@ -5,14 +5,25 @@ const app = require('../lib/app');
 const Tweet = require('../lib/models/Tweet');
 const User = require('../lib/models/User');
 
+// custom validator for tweet-by-id test's ID validation
+// var validateId = new Schema({
+//   _id: {
+//     type: 
+//   }
+// });
+
+// const idVal = function({
+
+// });
+
 describe('tweet routes testing', () => {
 
-  const createTweet = () => {
-    return User.create({ handle: 'hiding', name: 'underTheBed', email: 'come@out.com' })
-      .then(user => {
-        return Tweet.create({ user: user._id, body: 'The Brave Little Tweet' });
-      });
-  };
+  // const createTweet = () => {
+  //   return User.create({ handle: 'hiding', name: 'underTheBed', email: 'come@out.com' })
+  //     .then(user => {
+  //       return Tweet.create({ user: user._id, body: 'The Brave Little Tweet' });
+  //     });
+  // };
 
   beforeAll(() => {
     return mongoose.connect(process.env.MONGODB_URI, {
@@ -57,46 +68,30 @@ describe('tweet routes testing', () => {
       });
   });
 
-  it('can get a tweet by id', () => {
-    const usury = {
-      handle: 'hiding',
-      name: 'underTheBed',
-      _id: expect.anything(), //any(mongoose.Types.ObjectId)
-      __v: 0
-    };
-    return createTweet()
-      .then(createdTweet => {
-        return request(app)
-          .get(`/tweets/${createdTweet._id}`);
-      })
-      .then(res => {
-        expect(res.body).toEqual({
-          usury,
-          body: 'The Brave Little Tweet',
-          _id: expect.any(mongoose.Types.ObjectId)
-          
-        });
-      });
-  });
-
-  // original
   // it('can get a tweet by id', () => {
+          
+  //   const user = {
+  //     handle: 'hiding',
+  //     name: 'underTheBed',
+  //     _id: expect.anything(), //any(mongoose.Types.ObjectId)
+  //   };
   //   return createTweet()
   //     .then(createdTweet => {
   //       return request(app)
   //         .get(`/tweets/${createdTweet._id}`);
-  //     })
+  //       .then(res => {
+  //         res.toString();
+  //         return res;
+  //       });
+  //     }) 
   //     .then(res => {
+  //       //const noJsonIdTweet = res._id.toJSON();
   //       expect(res.body).toEqual({
-  //         user: {
-  //           handle: 'hiding',
-  //           name: 'underTheBed',
-  //           _id: expect.anything(), //any(mongoose.Types.ObjectId)
-  //           __v: 0
-  //         },
+  //         user,
   //         body: 'The Brave Little Tweet',
-  //         _id: expect.any(mongoose.Types.ObjectId)
-          
+  //         _id: expect.any(String),  //mongoose.Types.ObjectId),
+  //         __v: 0
+              
   //       });
   //     });
   // });
