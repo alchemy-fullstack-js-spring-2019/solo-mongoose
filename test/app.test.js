9a6d9ff1b49a2fce09b9988210c8d569b9a7d35a
Anna Ridenour <annamridenour@gmail.com>
2019-04-17 17:06:58 -0700

    relationships updated, tests all passing

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,7 @@ const Tweet = require('../lib/models/Tweet');
 const User = require('../lib/models/User');
 
 describe('tweet routes', () => {
+    
     beforeAll(() => {
         return mongoose.connect('mongodb://localhost:27017/tweets', {
             useNewUrlParser: true,
@@ -12,27 +13,32 @@ describe('tweet routes', () => {
             useCreateIndex: true
         });
     });
+
     beforeEach(() => {
         return mongoose.connection.dropDatabase();
     });
+
     afterAll(() => {
         return mongoose.connection.close();
     });
 
     it('creates a tweet', () => {
-        return request(app)
-            .post('/tweet')
-            .send({ handle: 'Anna', body: 'this is my tweet' })
+        return User.create({ handle: 'handle', name: 'name', email: 'email' })
+            .then(user => {
+                return request(app)
+                    .post('/tweet')
+                    .send({ user: user._id, body: 'this is my tweet' });
+            })
             .then(res => {
-                expect(res.body).toEqual({ handle: 'Anna', body: 'this is my tweet', _id: expect.any(String), __v: 0 });
+                expect(res.body).toEqual({ user: expect.any(String), body: 'this is my tweet', _id: expect.any(String), __v: 0 });
             });
     });
 
     it('gets a list of all tweets', () => {
-        return Tweet.create({ 
-            handle: 'allTweets', 
-            body: 'gotta list em all' 
-        })
+        return User.create({ handle: 'handle', name: 'name', email: 'email' })
+            .then(user => {
+                return Tweet.create({ user: user._id, body: 'this is my tweet' });
+            })
             .then(() => {
                 return request(app)
                     .get('/tweet');
@@ -43,36 +49,36 @@ describe('tweet routes', () => {
     });
 
     it('gets a tweet by id', () => {
-        return Tweet.create({ 
-            handle: 'tweetById', 
-            body: 'gotta list just one' 
-        })
+        return User.create({ handle: 'handle', name: 'name', email: 'email' })
+            .then(user => {
+                return Tweet.create({ user: user._id, body: 'this is my tweet' });
+            })
             .then(tweet => {
                 return request(app)
                     .get(`/tweet/${tweet._id}`);
             })
             .then(res => {
                 expect(res.body).toEqual({
-                    handle: 'tweetById', 
-                    body: 'gotta list just one',
-                    _id: expect.any(String),
+                    user: { handle: 'handle', name: 'name', email: 'email', _id: expect.any(String) },
+                    body: 'this is my tweet',
+                    _id: expect.any(String)
                 });
             });
     });
 
     it('updates a tweet', () => {
-        return Tweet.create({ 
-            handle: 'name', 
-            body: 'not updated' 
-        })
+        return User.create({ handle: 'handle', name: 'name', email: 'email' })
+            .then(user => {
+                return Tweet.create({ user: user._id, body: 'this is my tweet' });
+            })
             .then(tweet => {
                 return request(app)
                     .patch(`/tweet/${tweet._id}`)
                     .send({ body: 'updated' });
             })
             .then(res => {
-                expect(res.body).toEqual({
-                    handle: 'name', 
+                expect(res.body).toEqual({ 
+                    user: { handle: 'handle', name: 'name', email: 'email', _id: expect.any(String) }, 
                     body: 'updated',
                     _id: expect.any(String),
                 });
@@ -80,18 +86,18 @@ describe('tweet routes', () => {
     });
 
     it('deletes a tweet', () => {
-        return Tweet.create({ 
-            handle: 'name', 
-            body: 'delete me' 
-        })
+        return User.create({ handle: 'handle', name: 'name', email: 'email' })
+            .then(user => {
+                return Tweet.create({ user: user._id, body: 'this is my tweet' });
+            })
             .then(tweet => {
                 return request(app)
                     .delete(`/tweet/${tweet._id}`);
             })
             .then(res => {
                 expect(res.body).toEqual({ 
-                    handle: 'name', 
-                    body: 'delete me',
+                    user: { handle: 'handle', name: 'name', email: 'email', _id: expect.any(String) }, 
+                    body: 'this is my tweet',
                     _id: expect.any(String)
                 });
             });
